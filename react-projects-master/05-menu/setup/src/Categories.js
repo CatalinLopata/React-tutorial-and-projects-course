13ea6464ae5ea9205ff26fd7a979f5a99c784d4b
Catalin Lopata | eMAG <catalin.lopata@emag.ro>
2021-10-19 16:47:36 +0300

    menu project done

--- react-projects-master/05-menu/setup/src/Categories.js
+++ react-projects-master/05-menu/setup/src/Categories.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({ filterItems, categories }) => {
+
+  console.log(categories)
+
+  return (
+    <div className="btn-container">
+      {categories.map((category, index) => {
+        return <button type="button" className="filter-btn" ket={index} onClick={() => filterItems(category)}>{category}</button>
+      })}
+    </div>
+  )
 };
 
 export default Categories;
