f0611c5c7eb5eb0221aff36c53dd983903773b19
Catalin Lopata | eMAG <catalin.lopata@emag.ro>
2021-11-16 16:57:56 +0200

    use reducer

---
+++ react-advanced-2020-master/src/tutorial/6-useReducer/setup/reducers.js
@@ -0,0 +1,31 @@
+// reducer function
+export const reducer = (state, action) => {
+    console.log(state, action)
+    if (action.type === "ADD_ITEM") {
+        const newPeople = [...state.people, action.payload]
+        return {
+            ...state,
+            people: newPeople,
+            isModalOpen: true,
+            modalContent: "item added"
+        }
+    } else if (action.type === 'NO_VALUE') {
+        return {
+            ...state,
+            isModalOpen: true,
+            modalContent: "please insert a value"
+        }
+    } else if (action.type === 'CLOSE_MODAL') {
+        return {
+            ...state,
+            isModalOpen: false
+        }
+    } else if (action.type === 'REMOVE_ITEM') {
+        const newPeople = state.people.filter((person) => person.id !== action.payload)
+        return { ...state, people: newPeople }
+
+    }
+
+
+    throw new Error("no matching action type")
+}
