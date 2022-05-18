2686dd6f7c107591c6765eb76fcf622166ee13ea
Catalin Lopata | eMAG <catalin.lopata@emag.ro>
2022-05-18 14:02:14 +0300

    learn something new

--- react-projects-master/19-stock-photos/setup/src/Photo.js
+++ react-projects-master/19-stock-photos/setup/src/Photo.js
@@ -1,7 +1,18 @@
 import React from 'react'
 
-const Photo = () => {
-  return <h2>photo component</h2>
+const Photo = ({urls:{regular}, alt_description, likes, user: {name, portofolio_url, profile_image: {medium} } }) => {
+  return <article className='photo'>
+    <img src={regular} alt={alt_description} ></img>
+    <div className='photo-info'>
+      <div>
+        <h4>{name}</h4>
+        <p>{likes} likes</p>
+      </div>
+      <a href={portofolio_url}>
+        <img src={medium} alt={name} className="user-img"></img>
+      </a>
+    </div>
+  </article>
 }
 
 export default Photo
